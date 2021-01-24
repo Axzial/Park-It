6ecb0134b20f8d1b8109951b149c2defed111ebb
Axzial <axzial@protonmail.com>
2021-01-24 18:32:30 +0100

    :art: Using @Data on models

--- src/main/java/com/parkit/parkingsystem/model/ParkingSpot.java
+++ src/main/java/com/parkit/parkingsystem/model/ParkingSpot.java
@@ -1,8 +1,11 @@
 package com.parkit.parkingsystem.model;
 
 import com.parkit.parkingsystem.constants.ParkingType;
+import lombok.Data;
 
+@Data
 public class ParkingSpot {
+
     private int number;
     private ParkingType parkingType;
     private boolean isAvailable;
@@ -13,40 +16,4 @@ public class ParkingSpot {
         this.isAvailable = isAvailable;
     }
 
-    public int getId() {
-        return number;
-    }
-
-    public void setId(int number) {
-        this.number = number;
-    }
-
-    public ParkingType getParkingType() {
-        return parkingType;
-    }
-
-    public void setParkingType(ParkingType parkingType) {
-        this.parkingType = parkingType;
-    }
-
-    public boolean isAvailable() {
-        return isAvailable;
-    }
-
-    public void setAvailable(boolean available) {
-        isAvailable = available;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ParkingSpot that = (ParkingSpot) o;
-        return number == that.number;
-    }
-
-    @Override
-    public int hashCode() {
-        return number;
-    }
 }
