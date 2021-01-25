d4cf14a239b779e16dec2ee7f9552c701dae6635
Axzial <axzial@protonmail.com>
2021-01-25 18:10:50 +0100

    :wastebasket: Cleaning

--- src/main/java/com/parkit/parkingsystem/model/ParkingSpot.java
+++ src/main/java/com/parkit/parkingsystem/model/ParkingSpot.java
@@ -5,15 +5,9 @@ import lombok.Data;
 
 @Data
 public class ParkingSpot {
+
     private int id;
     private ParkingType parkingType;
     private boolean isAvailable;
-
-    public ParkingSpot(int id, ParkingType parkingType, boolean isAvailable) {
-        this.id = id;
-        this.parkingType = parkingType;
-        this.isAvailable = isAvailable;
-    }
-
 
 }
