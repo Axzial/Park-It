543cee9f89b2c59794ce60d2d148b3e43c17d7dd
Axzial <axzial@protonmail.com>
2021-01-25 18:10:18 +0100

    :zap: Refractor TicketDAO

--- src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
@@ -16,34 +16,40 @@ import java.sql.Timestamp;
 @Log4j
 public class TicketDAO extends AbstractDAO<Ticket> {
 
-    public boolean saveTicket(Ticket ticket) {
-        try (Connection con = dataBaseConfig.getConnection()) {
-            PreparedStatement ps = con.prepareStatement(DBRequest.SAVE_TICKET.getRequest());
-            //ID, PARKING_NUMBER, VEHICLE_REG_NUMBER, PRICE, IN_TIME, OUT_TIME)
-            //ps.setInt(1,ticket.getId());
+    /**
+     * Save a ticket
+     * @param ticket {@link Ticket}
+     * @return {@link Ticket}
+     */
+    public Ticket saveTicket(Ticket ticket) {
+        try (Connection con = dataBaseConfig.getConnection();
+             PreparedStatement ps = con.prepareStatement(DBRequest.SAVE_TICKET.getRequest());
+        ) {
             ps.setInt(1, ticket.getParkingSpot().getId());
             ps.setString(2, ticket.getVehicleRegNumber());
             ps.setDouble(3, ticket.getPrice());
             ps.setTimestamp(4, new Timestamp(ticket.getInTime().getTime()));
             ps.setTimestamp(5, (ticket.getOutTime() == null) ? null : (new Timestamp(ticket.getOutTime().getTime())));
-            return ps.execute();
+            return ticket;
         } catch (Exception ex) {
             log.error("Error fetching next available slot", ex);
         }
-        return false;
+        return ticket;
     }
 
+    /**
+     * Get a ticket by reg number
+     * @param vehicleRegNumber
+     * @return {@link Ticket}
+     */
     public Ticket getTicket(String vehicleRegNumber) {
-        Connection con = null;
-        Ticket ticket = null;
-        try {
-            con = dataBaseConfig.getConnection();
+        try(Connection con = dataBaseConfig.getConnection();
             PreparedStatement ps = con.prepareStatement(DBRequest.GET_TICKET.getRequest());
-            //ID, PARKING_NUMBER, VEHICLE_REG_NUMBER, PRICE, IN_TIME, OUT_TIME)
+        ) {
             ps.setString(1, vehicleRegNumber);
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
-                ticket = new Ticket();
+                Ticket ticket = new Ticket();
                 ParkingSpot parkingSpot = new ParkingSpot(rs.getInt(1), ParkingType.valueOf(rs.getString(6)), false);
                 ticket.setParkingSpot(parkingSpot);
                 ticket.setId(rs.getInt(2));
@@ -53,21 +59,23 @@ public class TicketDAO extends AbstractDAO<Ticket> {
                 ticket.setOutTime(rs.getTimestamp(5));
             }
             dataBaseConfig.closeResultSet(rs);
-            dataBaseConfig.closePreparedStatement(ps);
         } catch (Exception ex) {
             log.error("Error fetching next available slot", ex);
-        } finally {
-            dataBaseConfig.closeConnection(con);
-            return ticket;
         }
+        return new Ticket();
     }
 
+    /**
+     * Update a ticket in the database
+     * @param ticket
+     * @param <S>
+     * @return
+     */
     @Override
     public <S extends Ticket> S update(S ticket) {
-        Connection con = null;
-        try {
-            con = dataBaseConfig.getConnection();
+        try(Connection con = dataBaseConfig.getConnection();
             PreparedStatement ps = con.prepareStatement(DBRequest.UPDATE_TICKET.getRequest());
+        ) {
             ps.setDouble(1, ticket.getPrice());
             ps.setTimestamp(2, new Timestamp(ticket.getOutTime().getTime()));
             ps.setInt(3, ticket.getId());
@@ -75,8 +83,6 @@ public class TicketDAO extends AbstractDAO<Ticket> {
             return ticket;
         } catch (Exception ex) {
             log.error("Error saving ticket info", ex);
-        } finally {
-            dataBaseConfig.closeConnection(con);
         }
         return ticket;
     }
