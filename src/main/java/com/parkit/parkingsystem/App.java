785e92d52a661e68ca2886fdbb54c371479eb53a
Axzial <axzial@protonmail.com>
2021-02-27 15:38:02 +0100

    :wastebasket: Clean

--- src/main/java/com/parkit/parkingsystem/App.java
+++ src/main/java/com/parkit/parkingsystem/App.java
@@ -1,11 +1,7 @@
 package com.parkit.parkingsystem;
 
-import com.parkit.parkingsystem.dao.AbstractDAO;
-import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.service.InteractiveShell;
 import lombok.extern.log4j.Log4j;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Log4j
 public class App {
