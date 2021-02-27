2ecc9885701c3a2492f333763e957c450042b489
Axzial <axzial@protonmail.com>
2021-02-27 15:36:52 +0100

    :sparkles: FareCalculatorService now use Boolean for discount + free 30 minutes impl

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,14 +1,28 @@
 package com.parkit.parkingsystem.service;
 
 import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
+    public final TicketDAO ticketDAO = new TicketDAO();
 
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());//??
+    public static final double RECURRENT_DISCOUNT = 0.05;
+    public static final int FREE_MIN_DURATION = 30;
+
+
+
+
+    /**
+     * /!\ This method mutates price field of ticket
+     *
+     * @param ticket
+     */
+    public void calculateFare(Ticket ticket, Boolean isDiscounted) {
+
+        if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
+            throw new IllegalArgumentException("Out time provided is incorrect:");
         }
 
         long in = ticket.getInTime().getTime();
@@ -16,16 +30,27 @@ public class FareCalculatorService {
 
         long minutes = ((out - in) / 1000) / 60;
 
-        switch (ticket.getParkingSpot().getParkingType()){
+        //FREE 30 MINUTES
+        if (minutes <= FREE_MIN_DURATION) {
+            ticket.setPrice(0);
+            return;
+        }
+
+        //DISCOUNT ?
+        double discount = isDiscounted ? RECURRENT_DISCOUNT : 0;
+
+        //FINALLY
+        switch (ticket.getParkingSpot().getParkingType()) {
             case CAR: {
-                ticket.setPrice(minutes * (Fare.CAR_RATE_PER_HOUR.getPriceHour() / 60));
+                ticket.setPrice(minutes * (Fare.CAR_RATE_PER_HOUR.getPriceHour() / 60) * (1 - discount));
                 break;
             }
             case BIKE: {
-                ticket.setPrice(minutes * (Fare.BIKE_RATE_PER_HOUR.getPriceHour() / 60));
+                ticket.setPrice(minutes * (Fare.BIKE_RATE_PER_HOUR.getPriceHour() / 60) * (1 - discount));
                 break;
             }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
+            default:
+                throw new IllegalArgumentException("Unkown Parking Type");
         }
     }
 }
